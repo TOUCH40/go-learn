6b787d1b569761ca1b854809fe19bbbac48ad596
touch <396223353@qq.com>
2020-05-17 11:49:27 +0800

    unit12-13

---
+++ 12_if_eles-if_else/04_init-statement_error_invalid-code/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	b := true
+
+	if food, fuddo := "Chocolate", 2; b {
+		fmt.Println(food, fuddo)
+	}
+
+	//fmt.Println(food)
+	fmt.Println(fuddo)
+}
