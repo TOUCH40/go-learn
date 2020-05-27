98cb50a7132b6f3cc36054c2c3d319d365dfdca6
touch <396223353@qq.com>
2020-05-27 23:41:49 +0800

    99_01加入配置文件和调通读写，下一步添加正则验证

--- 99_my-exercise/01_file_operation/01_filter_files/main.go
+++ 99_my-exercise/01_file_operation/01_filter_files/main.go
@@ -3,13 +3,31 @@ package main
 import (
 	// "bufio"
 	"fmt"
+
 	// "io"
 	// "os"
 	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
 )
 
+type config struct {
+	Path string `yaml:"path"`
+}
+
 func main() {
-	ss, err := getFileFullNames(`F:\download`)
+	configFile, err := ioutil.ReadFile(`conf.yaml`)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	conf := new(config)
+	err = yaml.Unmarshal(configFile, conf)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	ss, err := getFileFullNames(conf.Path)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -28,9 +46,15 @@ func getFileFullNames(dirPath string) ([]string, error) {
 	}
 	for _, fileInfo := range files {
 		if fileInfo.IsDir() {
-			getFileFullNames(fileInfo.Name())
+			s2, err2 := getFileFullNames(dirPath + `\` + fileInfo.Name())
+			if err2 != nil {
+				fmt.Println(err2.Error())
+			} else {
+				s = append(s, s2...)
+			}
+
 		} else {
-			s = append(s, fileInfo.Name())
+			s = append(s, dirPath+`\`+fileInfo.Name())
 		}
 	}
 	return s, nil
