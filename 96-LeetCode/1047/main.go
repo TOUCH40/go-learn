fd9554eb52fc7aa2e0fd9a8b2d2bf43e3275502e
touch <396223353@qq.com>
2021-03-17 16:39:28 +0800

    111

--- 96-LeetCode/1047/main.go
+++ 96-LeetCode/1047/main.go
@@ -9,9 +9,13 @@ func main() {
 }
 
 func removeDuplicates(S string) string {
-	var a = ""
-	for _, _s := range S {
-		a += _s
+	stack := []byte{}
+	for i := range S {
+		if len(stack) != 0 && stack[len(stack)-1] == S[i] {
+			stack = stack[:len(stack)-1] // 出栈
+		} else {
+			stack = append(stack, S[i])
+		}
 	}
-	return a
+	return string(stack)
 }
