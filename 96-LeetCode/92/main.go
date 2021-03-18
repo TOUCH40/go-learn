197893dcaec1932eaecdbda580641c3121813fcc
touch <396223353@qq.com>
2021-03-19 07:54:35 +0800

    11

---
+++ 96-LeetCode/92/main.go
@@ -0,0 +1,21 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+
+}
