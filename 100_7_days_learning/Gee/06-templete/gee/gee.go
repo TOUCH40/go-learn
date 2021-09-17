24d25d80bc752bddf84b57c95cc49a80b938feed
touch <396223353@qq.com>
2021-09-17 18:11:55 +0800

    第三个函数

---
+++ 100_7_days_learning/Gee/06-templete/gee/gee.go
@@ -0,0 +1,105 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+	"path"
+	"strings"
+)
+
+type HandlerFunc func(*Context)
+
+type (
+	RouterGroup struct {
+		prefix     string
+		middleware []HandlerFunc // support middleware
+		// parent     *RouterGroup  // support nesting
+		engine *Engine // all groups share a Engine instance
+	}
+	Engine struct {
+		*RouterGroup
+		router *router
+		groups []*RouterGroup // store all group
+	}
+)
+
+// new is the constructor of gee.Engine
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+// Group is defined to create a New RouterGroup
+// Remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		// parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middleware...)
+		}
+	}
+	c := newContext(w, req)
+	c.handlers = middlewares
+	engine.router.handle(c)
+}
+
+// Use is defined to add middleware to the group
+func (group *RouterGroup) Use(middleware ...HandlerFunc) {
+	group.middleware = append(group.middleware, middleware...)
+}
+
+// new feature
+
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		// check if file exists and/or if we have permission to access it
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+		fileServer.ServeHTTP(c.Write, c.req)
+	}
+}
+
+func (group *RouterGroup) Static(relativePath, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	// Register GET handlers
+	group.GET(urlPattern, handler)
+}
---
+++ 100_7_days_learning/Gee/06-templete/main.go
@@ -0,0 +1,50 @@
+// 分组
+// 支持分组嵌套
+// middleware//，作用在主分组上的中间件也会作用在子分组
+// 子分组还应该带有自己的中间件
+// 提供强大的扩展能力
+// 还可以使用装饰器，更为灵活，效率未知
+package main
+
+import (
+	"gee"
+	"log"
+	"net/http"
+	"time"
+)
+
+func onlyForV2() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occured
+		c.Fail(500, "Internal Server Error")
+		// caculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
+func main() {
+	r := gee.New()
+	//
+	// r.Static("/assets","/usr/geektutu/blog/static")
+	r.Static("/assets", "./static")
+	//
+	r.Use(gee.Logger()) // golobal  middleware
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+	v2 := r.Group("/v2")
+	{
+		v2.Use(onlyForV2())
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+	}
+
+	r.Run(":8000")
+}
